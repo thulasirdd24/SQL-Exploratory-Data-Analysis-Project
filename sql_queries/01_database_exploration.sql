fafb0c6b0e7476c43610bfa55d0195d371d6c3b6
Thulasi Allampati <109816948+thulasirdd24@users.noreply.github.com>
2026-03-05 16:55:59 -0500

    Add query to explore columns in 'dim_customers' table
    Added query to explore all columns in the 'dim_customers' table.

--- sql_queries/01_database_exploration.sql
+++ sql_queries/01_database_exploration.sql
@@ -1,3 +1,9 @@
 -- Explore database tables
 SELECT *
 FROM INFORMATION_SCHEMA.TABLES;
+
+-- Explore All columns in the Database
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS; 
+WHERE TABLE_NAME = 'dim_customers'
+
