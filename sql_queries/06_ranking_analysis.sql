dbacaf785263221570baec1d5264b4518935baf8
Thulasi Allampati <109816948+thulasirdd24@users.noreply.github.com>
2026-03-05 17:03:47 -0500

    Enhance ranking analysis SQL queries
    Added queries to analyze top and worst-performing products based on revenue.

--- sql_queries/06_ranking_analysis.sql
+++ sql_queries/06_ranking_analysis.sql
@@ -1,5 +1,32 @@
-SELECT TOP 5 product_name,
-SUM(quantity) AS total_quantity
-FROM sales
-GROUP BY product_name
-ORDER BY total_quantity DESC;
+-- WHICH 5 PRODUCTS GENERATE THE HIGHEST REVENUE?
+SELECT TOP 5
+p.subcategory,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.subcategory
+ORDER BY total_revenue DESC
+
+SELECT *
+FROM(	
+	SELECT 
+	p.product_name,
+	SUM(f.sales_amount) AS total_revenue,
+	RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+	GROUP BY p.product_name)t
+WHERE rank_products <= 5
+	
+
+-- WHAT ARE THE 5 WORST-PERFORMING PRODUCTS IN TERMS OF SALES?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue ASC
